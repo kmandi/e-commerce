048242c7ed081bb9ea701a105d7d3e40672d2041
kmandi <kartickmandi702@gmail.com>
2022-12-26 03:08:20 +0530

    update and add password resert and confirm

--- MyApp/forms.py
+++ MyApp/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth import password_validation
-from django.contrib.auth.forms import (AuthenticationForm, PasswordChangeForm,
-                                       UserCreationForm, UsernameField)
+from django.contrib.auth.forms import (AuthenticationForm, PasswordChangeForm, SetPasswordForm,
+                                       PasswordResetForm, UserCreationForm,
+                                       UsernameField)
 from django.contrib.auth.models import User
 from django.utils.translation import gettext
 from django.utils.translation import gettext_lazy as _
@@ -12,7 +13,8 @@ class CustomerRegistrationForm(UserCreationForm):
         label="Password", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label="Conform Password (again)",
                                 widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    email = forms.CharField(required=True, widget=forms.EmailInput(
+    email = forms.CharField(
+            required=True, widget=forms.EmailInput(
         attrs={'class': 'form-control'}))
 
     class Meta:
@@ -24,11 +26,76 @@ class CustomerRegistrationForm(UserCreationForm):
 
 
 class LoginForm(AuthenticationForm):
-    username = UsernameField(widget=forms.TextInput(attrs={"autofocus": True, 'class':'form-control'}))
-    password = forms.CharField(label=_('password'), strip=False, widget=forms.PasswordInput(
-        attrs={'autocomplete': 'current-password', 'class': 'form-control'}))
+    username = UsernameField(
+            widget=forms.TextInput(attrs={
+                "autofocus": True, 'class': 'form-control'
+                }
+            )
+        )
     
+    password = forms.CharField(
+            label=_('password'),
+            strip=False,
+            widget=forms.PasswordInput(attrs={
+                'autocomplete': 'current-password', 'class': 'form-control'
+                }
+            )
+        )
+
+
 class MyPasswordChangeForm(PasswordChangeForm):
-    old_password = forms.CharField(label=_('Old Password'), widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'autofocus': True, 'class': 'form-control'}))
-    new_password1 = forms.CharField(label=_('New Password'), strip=False, widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control'}), help_text=password_validation.password_validators_help_text_html())
-    new_password2 = forms.CharField(label=_('Confirm New Password'), strip=False, widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class':'form-control'}))
+    old_password = forms.CharField(
+            label=_('Old Password'),
+            widget=forms.PasswordInput(attrs={
+                'autocomplete': 'current-password', 'autofocus': True, 'class': 'form-control'
+                }
+            )
+        )
+    
+    new_password1 = forms.CharField(
+            label=_('New Password'),
+            strip=False,
+            widget=forms.PasswordInput(attrs={
+                'autocomplete': 'new-password', 'class': 'form-control'
+                }
+            ),
+            help_text=password_validation.password_validators_help_text_html())
+    
+    new_password2 = forms.CharField(
+            label=_('Confirm New Password'),
+            strip=False,
+            widget=forms.PasswordInput(attrs={
+                'autocomplete': 'new-password', 'class': 'form-control'
+                }
+            )
+        )
+
+
+class MyPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(
+            label=_('Email'),
+            max_length=254,
+            widget=forms.EmailInput(attrs={
+                'autocomplete': 'email', 'class': 'form-control'
+                }
+            )
+        )
+
+
+class MySetPasswordForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+            label=_('New Password'), strip=False, widget=forms.PasswordInput(attrs={
+                'autocomplete': 'new-password', 'class': 'form-control'
+                }
+            ),
+            help_text=password_validation.password_validators_help_text_html()
+        )
+    
+    new_password2 = forms.CharField(
+            label=_("New password confirmation"),
+            strip=False,
+            widget=forms.PasswordInput(attrs= {
+                "autocomplete": "new-password", 'class': 'form-control'
+                }
+            )
+        )
--- MyApp/urls.py
+++ MyApp/urls.py
@@ -5,7 +5,8 @@ from django.urls import path
 
 from MyApp import views
 
-from .forms import LoginForm, MyPasswordChangeForm
+from .forms import (LoginForm, MyPasswordChangeForm, MyPasswordResetForm,
+                    MySetPasswordForm)
 
 urlpatterns = [
     path('', views.ProductView.as_view(), name='home'),
@@ -33,11 +34,19 @@ urlpatterns = [
 
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
-    path('changepassword/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html', form_class=MyPasswordChangeForm), name='changepassword'),
+    path('changepassword/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone/'), name='changepassword'),
 
-    path('password_change_done/', auth_views.PasswordChangeView.as_view(template_name='app/passwordechangedone.html'), name='changepassword_done'),
+    path('passwordchangedone/', auth_views.PasswordChangeView.as_view(template_name='app/passwordechangedone.html'), name='passwordchangedone'),
 
-    path('registration/', views.CustomerRegistrationView.as_view(),
-         name='customerregistration'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
+
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html', form_class=MySetPasswordForm), name='password_reset_confirm'),
+
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name='password_reset_complete'),
+
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'), name='password_reset_done'),
+
+    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
+
     path('checkout/', views.checkout, name='checkout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
